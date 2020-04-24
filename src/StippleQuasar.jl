5566c0f74a56f03059f603959ea9238d5a536aae
Adrian Salceanu <e@essenciary.com>
2020-04-24 19:03:36 +0200

    Files generated by PkgTemplates

---
+++ src/StippleQuasar.jl
@@ -0,0 +1,5 @@
+module StippleQuasar
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StippleQuasar
+using Test
+
+@testset "StippleQuasar.jl" begin
+    # Write your own tests here.
+end
