ab585776ac56b07f82f6b24487044d782430d447
Frank Urbach <frank.urbach@gmx.de>
2020-10-09 15:38:44 +0200

    Page-container and Page (Quasar) added

--- src/Layout.jl
+++ src/Layout.jl
@@ -7,6 +7,8 @@ export layout, pageContainer
 
 Genie.Renderer.Html.register_normal_element("q__layout", context = @__MODULE__)
 Genie.Renderer.Html.register_normal_element("q__page__container", context = @__MODULE__)
+Genie.Renderer.Html.register_normal_element("q__page", context = @__MODULE__)
+
 
 function layout(args...;
                 wrap::Function = StippleUI.DEFAULT_WRAPPER,
@@ -16,12 +18,20 @@ function pageContainer(args...;
                 wrap::Function = StippleUI.DEFAULT_WRAPPER,
                 kwargs...)
 
+function page(args...;
+              wrap::Function = StippleUI.DEFAULT_WRAPPER,
+              kwargs...)
+
   wrap() do
     q__layout(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
   end
 
   wrap() do
     q__page__container(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
+  end
+
+  wrap() do
+    q__page(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
   end
 
 
