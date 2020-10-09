b5342aa0841cbff8e885f66a9501ede6dc78935e
Frank Urbach <frank.urbach@gmx.de>
2020-10-09 15:54:01 +0200

    Changed missprepared functions

--- src/Layout.jl
+++ src/Layout.jl
@@ -3,7 +3,7 @@ module Layout
 using Genie, Stipple, StippleUI, StippleUI.API
 import Genie.Renderer.Html: HTMLString, normal_element
 
-export layout, pageContainer
+export layout, pageContainer, page
 
 Genie.Renderer.Html.register_normal_element("q__layout", context = @__MODULE__)
 Genie.Renderer.Html.register_normal_element("q__page__container", context = @__MODULE__)
@@ -13,29 +13,26 @@ Genie.Renderer.Html.register_normal_element("q__page", context = @__MODULE__)
 function layout(args...;
                 wrap::Function = StippleUI.DEFAULT_WRAPPER,
                 kwargs...)
+     wrap() do
+      q__layout(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
+     end
+end
 
 function pageContainer(args...;
                 wrap::Function = StippleUI.DEFAULT_WRAPPER,
                 kwargs...)
+      wrap() do
+        q__page__container(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
+      end
+end
 
 function page(args...;
               wrap::Function = StippleUI.DEFAULT_WRAPPER,
               kwargs...)
-
-  wrap() do
-    q__layout(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
-  end
-
-  wrap() do
-    q__page__container(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
-  end
-
-  wrap() do
-    q__page(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
-  end
-
-
-
+      wrap() do
+        q__page(args...; attributes([kwargs...], StippleUI.API.ATTRIBUTES_MAPPINGS)...)
+      end
 end
+
 
 end
