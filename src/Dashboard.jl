0d072fc3379b5b19c3aff930408e7edbaa566764
Adrian Salceanu <essenciary@users.noreply.github.com>
2021-04-20 19:15:17 +0200

    v0.5 (support for Stipple v0.11 new reactive API)

--- src/Dashboard.jl
+++ src/Dashboard.jl
@@ -27,8 +27,8 @@ function dashboard(elemid, content::Union{String,Vector}; partial::Bool = true, 
     string("<$tagname>", content, "</$tagname>")
   end
 
-  kwargs = NamedTuple(delete!(Dict{Symbol,Any}(kwargs...), :id), :id, elemid)
-  # kwargs = NamedTuple(delete!(Stipple.OptDict(kwargs...), :id), :id, elemid) # todo: reenable
+  # kwargs = NamedTuple(delete!(Dict{Symbol,Any}(kwargs...), :id), :id, elemid)
+  kwargs = NamedTuple(delete!(Stipple.OptDict(kwargs...), :id), :id, elemid)
 
   Stipple.layout(Genie.Renderer.Html.div(content; kwargs...); partial=partial, title=title, class=class,
                                                               head_content=head_content, style=style, channel=channel)
