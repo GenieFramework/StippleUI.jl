35c3e690223675ba6827c0700e1c8fac16d8ebfe
Adrian Salceanu <adrian@adrians-mbp.lan>
2022-01-06 23:01:45 +0100

    Better header element

--- src/Headings.jl
+++ src/Headings.jl
@@ -4,15 +4,17 @@ import Genie, Stipple
 
 export heading
 
-function heading(title::String = "";
+function heading( title::String = "";
                   class::String = "",
-                  img::Union{String,Nothing} = nothing,
+                  img::Union{AbstractString,Nothing} = nothing,
                   h = Genie.Renderer.Html.h1(title, class="st-header__title text-h3"),
+                  content::Union{AbstractString,Nothing,Vector{AbstractString}} = nothing,
                   kwargs...)
 
   Genie.Renderer.Html.header(; class="$(class) st-header q-pa-sm", kwargs...) do; [
     img
     h
+    content
   ]end
 end
 
