5305e6c0592399345e46d8bdde99aebfb20760eb
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-03-12 14:11:38 +0530

    Added checkbox docs

--- src/Checkboxes.jl
+++ src/Checkboxes.jl
@@ -7,6 +7,31 @@ export checkbox
 
 register_normal_element("q__checkbox", context = @__MODULE__)
 
+"""
+The `checkbox` component is another basic element for user input. You can use this to supply a way for the user to toggle an option.
+
+# Examples
+
+```julia-repl
+julia> checkbox(label = "Apples", fieldname = :valone, dense = true, size = "xl")
+```
+
+> Supported Props
+
+# Label Props
+-----------
+
+| prop  | type  | Description | Examples |
+|---|---|---|---|
+| label  | String  | Label to display along the component (or use the default slot instead of this prop)  | `I agree with the Terms and Conditions`  |
+| leftlabel | Bool | Label (if any specified) should be displayed on the left side of the component |  |
+
+# Model Props
+------------
+| prop  | type  | Description | Examples |
+|---|---|---|---|
+| fieldname  | Symbol  | Model of the component  |  `false` `['car', 'building']` |
+"""
 function checkbox(label::String = "",
                   fieldname::Union{Symbol,Nothing} = nothing,
                   args...; kwargs...)
