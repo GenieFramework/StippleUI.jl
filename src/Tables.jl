b19fec841fdf14512e8cc9cca351b3cbf4c6997f
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-03-23 02:02:15 +0530

    Adding table docs

--- src/Tables.jl
+++ src/Tables.jl
@@ -23,6 +23,28 @@ Base.@kwdef mutable struct Column
   sortable::Bool = true
 end
 
+"""
+    Column(name::String, args...)
+
+----------
+# Examples
+----------
+
+```julia-repl
+julia> Column("x2", align = :right)
+```
+
+----------
+# Arguments
+----------
+
+* `required::Bool` - if we use `visiblecolumns`, this col will always be visible
+* `label::String` - label for header
+* `align::Symbol` - alignment for cell
+* `field::String` - row Object property to determine value for this column ex. `name`
+* `sortable::Bool` - tell `table` you want this column sortable
+
+"""
 function Column(name::String; args...)
   Column(name = name; args...)
 end
@@ -31,6 +53,17 @@ function Base.Symbol(v::Vector{Column}) :: Vector{Symbol}
   [Symbol(c.name) for c in v]
 end
 
+"""
+    DataTablePagination(sort_by::Symbol, descending::Bool, page::Int, row_per_page::Int)
+    
+----------
+# Examples
+----------
+
+```julia-repl
+julia> DataTablePagination(rows_per_page=50)
+```
+"""
 Base.@kwdef mutable struct DataTablePagination
   sort_by::Symbol = :desc
   descending::Bool = false
@@ -38,6 +71,29 @@ Base.@kwdef mutable struct DataTablePagination
   rows_per_page::Int = 10
 end
 
+"""
+    DataTableOptions(addid::Bool, idcolumn::String, columns::Union{Vector{Column},Nothing}, columnspecs::Dict{Union{String, Regex}, Dict{Symbol, Any}})
+    
+----------
+# Examples
+----------
+
+```julia-repl
+julia> DataTableOptions(columns = [Column("x1"), Column("x2", align = :right)])
+```
+
+### Columnspecs
+
+Usage for formating columns
+
+```julia-repl
+julia> import Stipple.opts
+julia> df = DataFrame(a = sin.(-π:π/10:π), b = cos.(-π:π/10:π), c = string.(rand(21)))
+julia> dt = DataTable(df)
+julia> dt.opts.columnspecs[r"^(a|b)$"] = opts(format = jsfunction(raw"(val, row) => `${100*val.toFixed(3)}%`"))
+julia> model.table[] = dt 
+```
+"""
 Base.@kwdef mutable struct DataTableOptions
   addid::Bool = false
   idcolumn::String = "ID"
@@ -45,11 +101,15 @@ Base.@kwdef mutable struct DataTableOptions
   columnspecs::Dict{Union{String, Regex}, Dict{Symbol, Any}} = Dict()
 end
 
+
 Base.@kwdef mutable struct DataTable{T<:DataFrames.DataFrame}
   data::T = DataFrames.DataFrame()
   opts::DataTableOptions = DataTableOptions()
 end
 
+"""
+    DataTable(data::T, opts::DataTableOptions = DataTableOptions())
+"""
 function DataTable(data::T) where {T<:DataFrames.DataFrame}
   DataTable(data, DataTableOptions())
 end
