6ff5c2e49b5a5642c20bb8949395c19659c87539
hhaensel <helmut.haensel@gmx.de>
2022-04-14 23:59:52 +0300

    updated csscolors

--- src/API.jl
+++ src/API.jl
@@ -253,16 +253,19 @@ vue_pure(elem::Symbol, args...; kwargs...) = vue(elem, args...; kwargs...)
     `csscolors(names, colors)`
     `csscolors(prefix, colors)`
 
-Construct a css string that defines colors to be used for styling quasar elements.
+Construct a css string that defines colors to be used for styling quasar elements;
+either as a value for the keyword argument `color` or as a classname with
+the resective prefix `text-` or `bg-`.
+(cf. [quasar docs](https://quasar.dev/style/color-palette))
 # Usage
-css = styles(csscolors(:stipple, [RGB(0.2, 0.4, 0.8), "#123456", RGBA(0.1, 0.2, 0.3, 0.5)]))
+css = style(csscolors(:stipple, [RGB(0.2, 0.4, 0.8), "#123456", RGBA(0.1, 0.2, 0.3, 0.5)]))
 
-ui() = css * dashboard(vm(model), class="container", [
+ui(model) = css * page(model, class="container, text-stipple-1", [
   btn("Hit me", @click(:pressed), color="stipple-3")
 ])
 """
 function csscolors(name, color::AbstractString)
-  ".text-$name { color: $color }\n.bg-$name   { background: $color !important}"
+  ".text-$name { color: $color !important}\n.bg-$name   { background: $color !important}"
 end
 
 csscolors(name, color::Colorant) = csscolors(name, "#" * hex(color, :auto))
