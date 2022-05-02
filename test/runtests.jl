0cf1b5b0f51e1d967bd61f291048a1b2d4e53d3b
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-05-02 19:26:09 +0530

    Cleanup and adding comments

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,35 +8,18 @@ using SafeTestsets, Test
     using Playwright
 
     include("components/Buttons.jl")
-    # model = Buttons.factory() 
-    # @info model.press_btn
 
     route("/") do
       Buttons.factory() |> Buttons.ui |> html
     end
     up()
 
-
-    # t1 = `julia16 --project -e 'using Pkg; include("components/Buttons.jl")'` |> run
-    # @sync `julia16 --project -e 'include("components/Buttons.jl")'` |> run
-  
-    # `open "http://127.0.0.1:8000/"` |> run
+    # trigger precompilation of Buttons.jl components for testing
     HTTP.get("http://127.0.0.1:8000/")
 
-    # BLOCK = quote
-    #   browser = p.firefox.launch(headless = false, slow_mo = 1000)
-    #   page = browser.new_page()
-    #   page.goto("http://127.0.0.1:8000/")
-    #   browser.close()
-    # end
-
-    # VAR = :p
-
-    # ptest(VAR, BLOCK)
-
     x = "Hello"
-
-    # include("tests_buttons.jl")
+    
+    # run test with playwright.jl i.e. tests_buttons.jl
     t1 = `julia --project -e 'using Pkg; include("tests_buttons.jl")' $x` |> run
   end
 end
