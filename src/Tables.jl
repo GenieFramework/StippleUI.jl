0462a0c2912fa1a2ad99c3ef9c2abba30b10bc67
hhaensel <helmut.haensel@merckgroup.com>
2022-06-22 09:54:19 +0200

    update doc string for selectrows!

--- src/Tables.jl
+++ src/Tables.jl
@@ -351,14 +351,21 @@ end
 
 """
     selectrows!(model::ReactiveModel, tablefield::Symbol, selectionfield::Symbol = Symbol(tablefield, "_selection"), args...)
-    selectrows!(dt::R{<:DataTable}, dt_selection::R{<:Vector{<:AbstractDict{String}}}, args...)
+    selectrows!(dt::R{<:DataTable}, dt_selection::R, args...)
 
-Select table rows of a model based on selection criteria. Selection syntax is identical to `rowselection`
+Select table rows of a model based on selection criteria. More information on selection syntax can be found in `rowselection`
 
 ```
-selectrows!(model.table, model.selection, "b", r"hello|World")
-selectrows!(model, :table, :selection, "b", r"hello|World")
-selectrows!(model, :table, "b", r"hello|World") # assumes the existence of a field `:table_selection`
+@reactive! mutable struct TableDemo <: ReactiveModel
+    @mixin table::TableWithPaginationSelection
+end
+
+model = init(TableDemo)
+model.table[] = DataTable(DataFrame(a = [1, 2, 4, 6, 8, 10], b = ["Hello", "world", ",", "hello", "sun", "!"]))
+
+selectrows!(model, :table, [1, 2, 6]) # assumes the existence of a field `:table_selection`
+selectrows!(model.table, model.table_selection, "b", r"hello|World"i)
+selectrows!(model, :table, :table_selection, "a", iseven)
 ```
 """
 function selectrows!(dt::R{<:DataTable}, dt_selection::R, args...)
