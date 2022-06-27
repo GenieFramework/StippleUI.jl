f360a8ff1db26023088072cbc4968a7e2e5e15b5
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-06-27 12:55:53 +0530

    Added timeline examples in timeline docs

--- src/Timelines.jl
+++ src/Timelines.jl
@@ -20,7 +20,40 @@ The `timeline` component displays a list of events in chronological order. It is
 ### View
 
 ```julia-repl
-julia> 
+julia> Html.div(class="q-px-lg q-pb-md", [
+            timeline(color="secondary", [
+            timelineentry("Timeline Heading", heading=true),
+            timelineentry(title="Event Title", subtitle="February 22, 1986", [
+            Html.div("Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum.")
+            ]),
+            timelineentry(title="Event Title", subtitle="February 21, 1986", icon="delete",[
+            Html.div("Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum.")
+            ]),
+            timelineentry(title="Event Title",
+            subtitle="February 22, 1986",
+            avatar="https://cdn.quasar.dev/img/avatar2.jpg", [
+            Html.div("Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum.")
+            ])
+            ])
+       ])
 ```
 
 -----------
@@ -48,7 +81,15 @@ end
 ### View
 
 ```julia-repl
-julia> 
+julia> timelineentry(title="Event Title", subtitle="February 21, 1986", icon="delete",[
+            Html.div("Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum.")
+       ])
 ```
 
 -----------
