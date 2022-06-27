9a3de1b74e0aaa531c4346d885fc31e3a7e88fb5
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-06-27 14:35:39 +0530

    Added example to Splitters

--- src/Splitters.jl
+++ src/Splitters.jl
@@ -14,10 +14,37 @@ register_normal_element("q__splitter", context = @__MODULE__)
 # Examples
 ----------
 
+### Model
+```julia-repl
+julia> @reactive mutable struct SplitterModel <: ReactiveModel
+            splitterM::R{Int} = 50
+       end
+```
+
 ### View
 ```julia-repl
-julia> 
-])
+julia> splitter(:splitterM, style="height: 400px", [
+        template("", "v-slot:before", [
+          Html.div(class="q-pa-md", [
+            Html.div("Before", class="text-h4 q-mb-md"),
+            Html.div("{{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+               Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus
+               obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.",
+              class = "q-my-md",
+              @recur(:"n in 20"), key! = "n")
+          ])
+        ]),
+        template("", "v-slot:after", [
+          Html.div(class="q-pa-md", [
+            Html.div("After", class="text-h4 q-mb-md")
+              Html.div("{{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+               Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus
+               obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.",
+              class = "q-my-md",
+              @recur(:"n in 20"), key! = "n")
+          ])
+        ])
+       ])
 ```
 
 ----------
