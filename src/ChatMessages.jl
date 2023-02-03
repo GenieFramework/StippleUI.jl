fbffc4fe015a88ec29b5b974637177ea467e44dc
Helmut Hänsel <helmut.haensel@gmx.de>
2023-02-03 09:08:31 +0100

    fix handling of string arguments in chatmessage()

--- src/ChatMessages.jl
+++ src/ChatMessages.jl
@@ -7,8 +7,15 @@ export chatmessage
 
 register_normal_element("q__chat__message", context = @__MODULE__)
 
-function chatmessage(text::Union{Symbol, String} = "",
+function chatmessage(text::Union{Symbol, Vector{<:AbstractString}, AbstractString} = "",
                     args...; kwargs...)
+  text = if text isa Symbol
+    text
+  elseif text isa String
+    Symbol("['$text']")
+  else
+    Symbol("['$(join(replace.(text, "'" => "\\'"), "', '"))']")
+  end
   q__chat__message(args...; kw([:text => text, kwargs...])...)
 end
 
