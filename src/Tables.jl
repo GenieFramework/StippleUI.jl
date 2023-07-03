ec46b935125d026db398a272bd32b184c3f50476
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-07-03 19:45:13 +0530

    DataTables docstring improvement

--- src/Tables.jl
+++ src/Tables.jl
@@ -124,6 +124,18 @@ end
 ```julia-repl
 julia> df = DataFrame(a = sin.(-π:π/10:π), b = cos.(-π:π/10:π), c = string.(rand(21)))
 julia> dt = DataTable(df)
+
+or
+
+julia> using TypedTables
+julia> t = Table(a = [1, 2, 3], b = [2.0, 4.0, 6.0])
+julia> dt = DataTable(t)
+
+or
+
+julia> using Tables
+julia> Tables.table([1 2 3; 3 4 5], header = ["a", "b", "c"])
+julia> dt = DataTable(t1)
 ```
 """
 function DataTable(data::T) where {T}
