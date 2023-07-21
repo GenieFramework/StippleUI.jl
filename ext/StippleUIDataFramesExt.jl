5fa80e16f084d6ec7f7100760323e2ce860f715d
Helmut Hänsel <helmut.haensel@gmx.com>
2023-07-21 11:53:59 +0200

    add DataTableWithSelection() in extension

--- ext/StippleUIDataFramesExt.jl
+++ ext/StippleUIDataFramesExt.jl
@@ -10,6 +10,10 @@ function StippleUI.QTables.DataTable()
   DataTable(DataFrames.DataFrame())
 end
 
+function StippleUI.DataTableWithSelection()
+  DataTableWithSelection(DataTable(),  DataTablePagination(), DataTableSelection())
+end
+
 function Stipple.convertvalue(target::Stipple.R{<:DataTable{DataFrames.DataFrame}}, d::AbstractDict)
   df = Stipple.stipple_parse(DataFrames.DataFrame, d["data"])
   DataTable(df[:, names(df) .!== "__id"], target.opts)
