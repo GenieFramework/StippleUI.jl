fe6f85fc4ad8b5b31415ba3588d982656a4d8f06
Adrian Salceanu <adrian@genieframework.com>
2024-08-28 16:08:59 +0200

    app2

---
+++ demos/tables/app2.jl
@@ -0,0 +1,43 @@
+module App
+
+using GenieFramework
+using DataFrames
+@genietools
+
+@app begin
+  NO_OF_ROWS = 1000
+  DATA = sort!(DataFrame(rand(NO_OF_ROWS, 2), ["x1", "x2"]))::DataFrame # we only sort so that the changes are more visible when filtering and paginating
+  ROWS_PER_PAGE = 10
+
+  @out df = DATA
+  @out dt = DataTable(DataFrame([]), DataTableOptions(DATA))
+  @out pagination = DataTablePagination(rows_per_page = ROWS_PER_PAGE, rows_number = NO_OF_ROWS)
+  @out loading = false
+  @in filter = ""
+
+  @onchange isready begin
+    dt = DataTable(DATA[1:ROWS_PER_PAGE, :])
+    filter = ""
+  end
+
+  @event request begin
+    loading = true
+    state = process_request(DATA, dt, pagination, filter)
+    dt = state.datatable
+    pagination = state.pagination
+    loading = false
+  end
+
+  @onchange filter begin
+    loading = true
+    pagination.page = 1
+    state = process_request(DATA, dt, pagination, filter)
+    dt = state.datatable
+    pagination = state.pagination
+    loading = false
+  end
+end
+
+@page("/", "ui.jl")
+
+end
