fca6192f5a760df74e053ed02ecec41aa0a6a381
hhaensel <helmut.haensel@gmx.com>
2026-09-02 14:40:12 +0200

    support js interpolation in notifications

--- src/Notifications.jl
+++ src/Notifications.jl
@@ -7,16 +7,31 @@ export notify
 """
     notify(model::ReactiveModel, message::AbstractString, type::Union{Symbol, String, Nothing} = nothing; kwargs...)
 
-Display a notification on the client, by using Quasar's `$q-notify`.
-Types are e.g. `:positive`, `:negative`, `:warning`, `:info`, `:ongoing`.
-All other arguments to `$q-notify` are supported via keyword arguments
+Display a notification on the client (via Quasar's `$q-notify`)
+
+Parameters:
+- `type` (optional): specifies the display type, e.g. `:positive`, `:negative`, `:warning`, `:info`, `:ongoing`.
+- `interpolation`: enables Vue interpolation syntax, values need to be prefixed by `"this"`, e.g.
+`"{{ this.name }}"`
+- Keyword arguments, e.g. `html`, `caption`, are passed 1:1 to Quasar's `$q-notify`, for details, see https://quasar.dev/quasar-plugins/notify
 
 # Example
-`notify(model, "Hello world!", :positive, icon = :tag_faces, caption = "5 minutes ago")`
+```
+notify(model, "Hello world!", :positive, icon = :tag_faces, caption = "5 minutes ago")
+notify(model, "Hello, I am {{ this.name }}", interpolation = true)
+```
 """
-function Base.notify(model::ReactiveModel, message::AbstractString, type::Union{Symbol, String, Nothing} = nothing; kwargs...)
-  d = Stipple.opts(;type, message, kwargs...)
-  js_dict = strip(json(d), '"')
+function Base.notify(model::ReactiveModel, message::AbstractString, type::Union{Symbol, String, Nothing} = nothing; interpolation = false, kwargs...)
+  if interpolation && (contains(message, "{{") || contains(message, "\${"))
+    dummy = "__message_dummy__"
+    d = filter!(x -> x[2] !== nothing, Stipple.opts(; type, message = dummy, kwargs...))
+    js_dict = strip(json(d), '"')
+    message = replace(message, "{{" => "\${", "}}" => "}")
+    js_dict = replace(js_dict, "\"$dummy\"" => "`$message`")
+  else
+    d = filter!(x -> x[2] !== nothing, Stipple.opts(; type, message, kwargs...))
+    js_dict = strip(json(d), '"')
+  end
   run(model, "window?.GENIEMODEL?.\$q.notify($js_dict)")
 end
 
